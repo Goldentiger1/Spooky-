9e482a21a2f6ca792a6e2d4efaae5be9b3ec15d6
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-06-14 10:51:04 +0300

    Tällä hetkellä oikeassa kädessä padia painaessa valo syttyy ja sammuu TESTI, scripti kansio oman skenen muokkaus

---
+++ Spooky/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    public SteamVR_TrackedController tc;
+    public Light flashlight;
+
+    void Update()
+    {
+        tc.PadClicked += SwitchOnOff;
+    }
+
+    public void SwitchOnOff(object sender, ClickedEventArgs e)
+    {
+        flashlight.enabled = !flashlight.enabled;
+    }
+}
