44475b351d0131b276120a0381390801cee0e14f
Mira <mira.kainulainen@hotmail.com>
2018-06-15 10:31:25 +0300

    Oma scene, jotain materiaaleja ja jonkunlainen Timer...

---
+++ Spooky/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text gameTime;
+    float gameTimer = 0f;
+
+
+	void Update () {
+        gameTimer += Time.deltaTime;
+
+        int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+        int hours = (int)(gameTimer / 3600) % 24;
+
+        string timerstring = string.Format("{0:0}:{1:00}:{2:00}",hours,minutes,seconds);
+
+        gameTime.text = timerstring;
+	}
+}
