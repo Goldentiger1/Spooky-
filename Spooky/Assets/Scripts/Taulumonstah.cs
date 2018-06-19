ddeb9bc9875bbfed0befa046a4735d13baa273cb
Mira <mira.kainulainen@hotmail.com>
2018-06-19 14:33:22 +0300

    monstahhh

--- Spooky/Assets/Scripts/Taulumonstah.cs
+++ Spooky/Assets/Scripts/Taulumonstah.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Taulumonstah : MonoBehaviour {
 
+    public GameObject target;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 targetDirection = target.transform.position - transform.position;
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        transform.rotation = Quaternion.RotateTowards(currentRotation, targetRotation, Time.deltaTime * 180);
+    }
 }
